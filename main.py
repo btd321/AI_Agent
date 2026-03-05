95ea90ff4ce78cdd136cda288c60429ccf991900
btd321 <btd321@gmail.com>
2026-03-05 17:43:05 -0500

    added verbose tag and messages log

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -12,16 +13,24 @@ def main():
     client = genai.Client(api_key=api_key)
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=args.user_prompt
+        contents=messages
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
     print(f'Response:')
     print(response.text)
-
 
 if __name__ == "__main__":
     main()
