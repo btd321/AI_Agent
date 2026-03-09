9411473f93173416ce8cb427a1a9081037843121
btd321 <btd321@gmail.com>
2026-03-08 22:52:33 -0400

    added run python function

---
+++ functions/run_python_file.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) 
+        if not valid_target_dir == working_dir_abs:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if file_path[(len(file_path)-3)::] != ".py":
+            return f'Error: "{file_path}" is not a Python file'
+        command = ["python", target_dir]
+        if args != None:
+            command.extend(args)
+        obj1 = subprocess.run(command,capture_output=True,cwd=working_dir_abs, timeout=30,text=True)
+        report_string = ""
+        string_list = []
+        if obj1.returncode != 0:
+            string_list.append(f"Process exited with code {obj1.returncode}")
+        if obj1.stdout == "" and obj1.stderr == "":
+            string_list.append("No output produced")
+        if obj1.stdout:
+            string_list.append(f"STDOUT:\n{obj1.stdout}")
+        if obj1.stderr:
+            string_list.append(f"STDERR:\n{obj1.stderr}")
+        report_string = "\n".join(string_list)
+        return report_string
+    except Exception as e:
+        return f"Error: executing Python file: {e}"    
