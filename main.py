fbe417c902c26413bc7f92198fa4985fb94f90c7
btd321 <btd321@gmail.com>
2026-03-10 10:59:21 -0400

    added system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 import argparse
 from google.genai import types
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -21,7 +22,8 @@ def main():
 
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
 
     if args.verbose:
