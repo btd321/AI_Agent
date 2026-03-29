346a9b4921eecc6b15b70eaa54ff5376325e2a1a
btd321 <btd321@gmail.com>
2026-03-28 22:54:17 -0400

    added agentic loop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 import argparse
@@ -21,39 +22,44 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
+    for _ in range(20):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash', 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            )
         )
-    )
-
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates:
+            for item in response.candidates:
+                messages.append(item.content)
+        
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
-    if response.function_calls == None:
-        print(f'Response:')
-        print(response.text)
-    else:
-        function_responses = []
-        for function_call in response.function_calls:
-            function_call_result = call_function.call_function(function_call,args.verbose)
-            if function_call_result.parts == None:
-                raise Exception   
-            if function_call_result.parts[0].function_response == None:
-                raise Exception
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception
-            function_responses.append(function_call_result.parts[0])
-            if args.verbose == True:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-
-     
-
+        if not response.function_calls:
+            print(f'Response:')
+            print(response.text)
+            return
+        else:
+            function_responses = []
+            for function_call in response.function_calls:
+                function_call_result = call_function.call_function(function_call,args.verbose)
+                if function_call_result.parts == None:
+                    raise Exception   
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception
+                function_responses.append(function_call_result.parts[0])
+                if args.verbose == True:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+            messages.append(types.Content(role="user", parts=function_responses))
+    if not response.text:
+        print(f"Maximum number of iterations reached")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
